8ee1a6db3d2d54d1920497c22be426b51ddabde6
Earth111 <sacha.dorival2@gmail.com>
2025-06-23 22:46:18 +0200

    upload files 23/06/2025

---
+++ Organisation du projet           2025-06-23.mm
@@ -0,0 +1,294 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1748943992815" ID="ID_1678072855" MODIFIED="1749084119683" TEXT="Organisation du projet">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1748944000141" ID="ID_170692113" MODIFIED="1749059557244" POSITION="right" TEXT="cartes mentales &#xe0; finir">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1749099250269" ID="ID_375449387" MODIFIED="1749099251612" TEXT="mars">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1749099251870" ID="ID_1703786706" MODIFIED="1749099253910" TEXT="sombrelys">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1749099254150" ID="ID_113226196" MODIFIED="1749099259169" TEXT="alchimie">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1749099259460" ID="ID_1313760533" MODIFIED="1749099270155" TEXT="animux">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1749099273649" ID="ID_622213389" MODIFIED="1749099276428" TEXT="qu&#xea;tes">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1749099280837" ID="ID_497060827" MODIFIED="1749099282182" TEXT="th&#xe8;mes">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1749099284354" HGAP="28" ID="ID_1540692587" MODIFIED="1749128996522" TEXT="tout fusionner" VSHIFT="32">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1749099241121" ID="ID_1087478798" MODIFIED="1749129021479" POSITION="right" TEXT="donjon du premier pixel" VSHIFT="47">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1748956146748" HGAP="25" ID="ID_1254748936" MODIFIED="1749129025702" POSITION="right" TEXT="s&apos;entra&#xee;ner &#xe0; la cr&#xe9;ativit&#xe9;" VSHIFT="39">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1748956162014" ID="ID_1773077318" MODIFIED="1749059557253" TEXT="&#xe9;crire des intros d&apos;histoire ou de roman">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748956173934" ID="ID_810425684" MODIFIED="1749059557261" TEXT="&#xe9;crire des histoire &#xe0; partir de simples mots ou id&#xe9;es">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1749099307008" ID="ID_492388945" MODIFIED="1749099321346" TEXT="demander chatgpt ou vid&#xe9;o sc&#xe9;narurgien">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1748944009115" ID="ID_732409920" MODIFIED="1749129041906" POSITION="left" TEXT="outils : codage / id&#xe9;es / texturing">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1748944018242" ID="ID_1522239007" MODIFIED="1749059557270" TEXT="chat gpt">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748944020712" ID="ID_59660671" MODIFIED="1749084121113" TEXT="blockbench">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1748944595008" ID="ID_1911162422" MODIFIED="1749084121117" TEXT="plug in ?">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1748944599770" ID="ID_716813324" MODIFIED="1749059557286" TEXT="Animation generator"/>
+<node COLOR="#111111" CREATED="1748944611177" ID="ID_262427190" MODIFIED="1749059557287" TEXT="entity wizard"/>
+<node COLOR="#111111" CREATED="1748944624897" ID="ID_826939284" MODIFIED="1749059557287" TEXT="UV toolkit"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1748944024340" ID="ID_1707040567" MODIFIED="1749084121119" TEXT="mcreator">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1748944637197" ID="ID_466268389" MODIFIED="1749084121121" TEXT="plug in ?">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1749129188773" ID="ID_1729388359" MODIFIED="1749129201766" TEXT="template + textures ?"/>
+<node COLOR="#111111" CREATED="1749129204645" ID="ID_666807805" MODIFIED="1749129206933" TEXT="autres ?"/>
+</node>
+<node COLOR="#990000" CREATED="1748944643645" ID="ID_428160357" MODIFIED="1749129186624" TEXT="plus de textures minecraft ?">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1748944653069" ID="ID_1256857769" MODIFIED="1749059557292" TEXT="templates ?"/>
+<node COLOR="#111111" CREATED="1748944655849" ID="ID_1251600476" MODIFIED="1749084121127" TEXT="extraire le .jar">
+<node COLOR="#111111" CREATED="1748944715194" ID="ID_1845120741" MODIFIED="1749059557293" TEXT=".minecraft/versions/[version].jar"/>
+<node COLOR="#111111" CREATED="1748944718253" ID="ID_1866007163" MODIFIED="1749059557294" TEXT="ouvrir avec 7zip"/>
+<node COLOR="#111111" CREATED="1748944742790" ID="ID_651582356" MODIFIED="1749059557294" TEXT="assets/minecraft/textures"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1748944026972" ID="ID_1244651553" MODIFIED="1749059557296" TEXT="nova skin ?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748944033410" ID="ID_1108435585" MODIFIED="1749059557297" TEXT="mc crayfish model creator ?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748944508725" ID="ID_1834062449" MODIFIED="1749059557298" TEXT="Paint . NET ?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748944529883" ID="ID_908875590" MODIFIED="1749059557302" TEXT="GIMP ?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748944539683" ID="ID_280560362" MODIFIED="1749059557305" TEXT="Pixlr / Piskel ?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1749121796213" FOLDED="true" HGAP="29" ID="ID_1965446850" MODIFIED="1749129160088" TEXT="Matt&#xe9;o" VSHIFT="140">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1749121798481" ID="ID_823484690" MODIFIED="1749121809070" TEXT="misode.github.io">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1749121810240" ID="ID_564546222" MODIFIED="1749129066506" TEXT="--&gt; datapack"/>
+</node>
+<node COLOR="#990000" CREATED="1749122051248" ID="ID_1385593579" MODIFIED="1749122053284" TEXT="VScode">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1749122054141" ID="ID_941623645" MODIFIED="1749122059549" TEXT="plug-in"/>
+<node COLOR="#111111" CREATED="1749122066362" ID="ID_1390184301" MODIFIED="1749122071872" TEXT="extension">
+<node COLOR="#111111" CREATED="1749122073084" ID="ID_1094372159" MODIFIED="1749122081189" TEXT="syntax-mcfunction"/>
+<node COLOR="#111111" CREATED="1749121813369" ID="ID_1051787107" MODIFIED="1749122117222" TEXT="spyglass mc">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1749122839149" ID="ID_1532350997" MODIFIED="1749122842562" TEXT="mc stacker">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1749125442897" HGAP="17" ID="ID_122542258" MODIFIED="1749125459956" TEXT="mc create live 5" VSHIFT="67">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1748956739138" FOLDED="true" ID="ID_75567089" MODIFIED="1749129221934" POSITION="left" TEXT="sons">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1748956742192" ID="ID_528612694" MODIFIED="1749059557309" TEXT=" BBC Sound Effect Library">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748956756871" ID="ID_602243681" MODIFIED="1749059557310" TEXT="Freesound.org">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748956766816" ID="ID_502508409" MODIFIED="1749084121135" TEXT="Sonus">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1748956769417" ID="ID_929034808" MODIFIED="1749059557313" TEXT="ia">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1748956772174" ID="ID_324186367" MODIFIED="1749059557319" TEXT="Voicemod AI">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748956796773" ID="ID_58630645" MODIFIED="1749059557320" TEXT="RAVE">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748956800562" ID="ID_568867094" MODIFIED="1749059557322" TEXT="DDSP">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748956807518" ID="ID_532565850" MODIFIED="1749059557323" TEXT="ElevenLabs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1748944455673" FOLDED="true" ID="ID_131092068" MODIFIED="1749129223973" POSITION="left" TEXT="YT">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1748944459088" ID="ID_1527429211" MODIFIED="1749059557328" TEXT="Zatocraft">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748944462965" ID="ID_1405567741" MODIFIED="1749059557329" TEXT="FloriannMC">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748944470467" ID="ID_843040287" MODIFIED="1749059557333" TEXT="SKyZz">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1748944476069" ID="ID_249747291" MODIFIED="1749059557335" TEXT="Diamond Developper">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1749030500872" ID="ID_1508335288" MODIFIED="1749059557337" TEXT="McHorse">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1749030518181" ID="ID_1545572788" MODIFIED="1749059557338" TEXT="NorthWestTrees gaming">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1749058879977" FOLDED="true" HGAP="18" ID="ID_543691597" MODIFIED="1749129036344" POSITION="left" TEXT="CODAGE PUR" VSHIFT="102">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1749058884000" ID="ID_807611713" MODIFIED="1749121757149" TEXT="priorit&#xe9; features">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1749058894051" ID="ID_711679879" MODIFIED="1749059557342" TEXT="data packs locaux du monde">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1749058900966" ID="ID_1496180636" MODIFIED="1749059557347" TEXT="data packs globaux">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1749058906572" ID="ID_418957375" MODIFIED="1749059557349" TEXT="mod pack">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1749058908669" ID="ID_678866741" MODIFIED="1749059557351" TEXT="vanilla">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1749064720750" ID="ID_781006869" MODIFIED="1749121757543" TEXT="fonctionnement data pack">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1749064726437" ID="ID_624995216" MODIFIED="1749064766050" TEXT="supprime ou modifie un &#xe9;l&#xe9;ment existant d&#xe9;j&#xe0; en vanilla">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1749064751441" ID="ID_712417802" MODIFIED="1749064775295" TEXT="utilisable dans minecraft vanilla">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1749064777296" ID="ID_1211117507" MODIFIED="1749084121155" TEXT="pour en cr&#xe9;er un">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1749064792996" ID="ID_942653440" MODIFIED="1749064823120" TEXT="cr&#xe9;er un fichier avec le m&#xea;me nom qu&apos;en vanilla"/>
+<node COLOR="#111111" CREATED="1749064825422" ID="ID_1836691421" MODIFIED="1749084121156" TEXT="modifier ce que l&apos;on veut dedans">
+<node COLOR="#111111" CREATED="1749064835023" ID="ID_1280184134" MODIFIED="1749064845898" TEXT="fonctionnement, voire m&#xea;me laisser vide"/>
+</node>
+<node COLOR="#111111" CREATED="1749064847335" ID="ID_824793247" MODIFIED="1749064899250" TEXT="modifier ce que cela implique pour les &#xe9;l&#xe9;ments qui contiennent cet &#xe9;l&#xe9;ment (structures, loot tables, recettes, ... )"/>
+<node COLOR="#111111" CREATED="1749064905056" ID="ID_115656959" MODIFIED="1749084121157" TEXT="compiler le tout en dossier data pack">
+<node COLOR="#111111" CREATED="1749064929643" ID="ID_1162818519" MODIFIED="1749064936890" TEXT="architecture ?"/>
+</node>
+<node COLOR="#111111" CREATED="1749064914350" ID="ID_1503842374" MODIFIED="1749064928013" TEXT="ins&#xe9;rer dans les fichiers du monde, ou dans le mod pack"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1749059587251" ID="ID_225482188" MODIFIED="1749084134788" TEXT="data pack &#xe0; faire">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1749059592970" ID="ID_37592137" MODIFIED="1749059604888" TEXT="modifier le fonctionnement du diamant">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1749059605814" ID="ID_1488938830" MODIFIED="1749059617085" TEXT="modifier des structures de base ?">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1749064702570" ID="ID_1267647301" MODIFIED="1749084135194" TEXT="mods &#xe0; faire">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1749064711068" ID="ID_1659571732" MODIFIED="1749064717738" TEXT="voir carte mentale g&#xe9;n&#xe9;rale">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1749082926900" HGAP="24" ID="ID_1247477712" MODIFIED="1749129008752" POSITION="right" TEXT="groupe whatsapp" VSHIFT="103">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1749082935128" ID="ID_789290028" MODIFIED="1749084151820" TEXT="carte mentale chaque semaine, avec changements versions  ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1749099227424" ID="ID_625709256" MODIFIED="1749099238003" TEXT="indiquer o&#xf9; se trouvent les changements">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1749082972282" ID="ID_949525473" MODIFIED="1749084121170" TEXT="bilan hebdomadaire">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1749082982025" ID="ID_1943782413" MODIFIED="1749084121172" TEXT="avancement dans le projet">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1749082990554" ID="ID_1061202627" MODIFIED="1749082995757" TEXT="nouvelles id&#xe9;es"/>
+<node COLOR="#111111" CREATED="1749082996173" ID="ID_1760656515" MODIFIED="1749083019462" TEXT="dans la partie informatique pure"/>
+</node>
+<node COLOR="#990000" CREATED="1749083020756" ID="ID_826819705" MODIFIED="1749083053281" TEXT="difficult&#xe9;s, probl&#xe8;mes et solutions">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1749083025095" ID="ID_1239350525" MODIFIED="1749083043763" TEXT="nouveaux logiciels, plug-in">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1749104919615" ID="ID_1443562362" MODIFIED="1749104925893" TEXT="blague de la semaine ?">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1749082947001" ID="ID_462617572" MODIFIED="1749082950595" TEXT="amis ?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
